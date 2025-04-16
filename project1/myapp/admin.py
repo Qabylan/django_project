a41bc9c179e4fffeee59580b5e176a7d9bda9018
Arman <armanalikhanuly@gmail.com>
2025-04-16 16:19:18 +0500

    Added models for diary enrty

--- project1/myapp/admin.py
+++ project1/myapp/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from .models import Task
-# Register your models here.
+from .models import Entry
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at') # Поля, которые будутотображаться в списке
-    search_fields = ('title',) # Поиск по названию задачи
-    ist_filter = ('created_at',) # Фильтр по дате создания
+admin.site.register(Entry)
--- project1/myapp/models.py
+++ project1/myapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Entry(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return 'Entry #{}'.format(self.id)
+
+    class Meta:
+        verbose_name_plural = 'entries'
